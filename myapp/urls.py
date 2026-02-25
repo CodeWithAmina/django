f935b26bdd288b037dd35975558ea01a2db0d8f8
Amina Muhammed <aminamuhammed9060@gmail.com>
2026-02-25 12:15:43 +0530

    crud

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,8 +1,11 @@
-from .views import simple_form
 from django.urls import path
+from .views import contact_create, contact_list, contact_detail, contact_update, contact_delete
 
 
 urlpatterns = [
-    path('forms/', simple_form,name='form'),
-    
+    path('contacts/', contact_list, name='contact_list'),
+    path('contacts/create/', contact_create, name='contact_create'),
+    path('contacts/<int:pk>/', contact_detail, name='contact_detail'),
+    path('contacts/<int:pk>/update/', contact_update, name='contact_update'),
+    path('contacts/<int:pk>/delete/', contact_delete, name='contact_delete'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,15 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .forms import SimpleForm
 from .models import Contact
 
 
-def simple_form(request):
-    form = SimpleForm()
-    if request.method=='POST':
-        form=SimpleForm(request.POST)
+# CREATE
+def contact_create(request):
+    if request.method == 'POST':
+        form = SimpleForm(request.POST)
         if form.is_valid():
-            name=form.cleaned_data['name']
-            email=form.cleaned_data['email']
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
             Contact.objects.create(name=name, email=email)
-            return render(request,"success.html",{'name':name})
-    return render(request,"form.html",{'form':form})
+            return redirect('contact_list')
+    else:
+        form = SimpleForm()
+    return render(request, 'contact_form.html', {'form': form})
+
+
+# READ - List all contacts
+def contact_list(request):
+    contacts = Contact.objects.all()
+    return render(request, 'contact_list.html', {'contacts': contacts})
+
+
+# READ - Get single contact
+def contact_detail(request, pk):
+    contact = get_object_or_404(Contact, pk=pk)
+    return render(request, 'contact_detail.html', {'contact': contact})
+
+
+# UPDATE
+def contact_update(request, pk):
+    contact = get_object_or_404(Contact, pk=pk)
+    if request.method == 'POST':
+        form = SimpleForm(request.POST)
+        if form.is_valid():
+            contact.name = form.cleaned_data['name']
+            contact.email = form.cleaned_data['email']
+            contact.save()
+            return redirect('contact_detail', pk=contact.pk)
+    else:
+        form = SimpleForm(initial={'name': contact.name, 'email': contact.email})
+    return render(request, 'contact_form.html', {'form': form, 'contact': contact})
+
+
+# DELETE
+def contact_delete(request, pk):
+    contact = get_object_or_404(Contact, pk=pk)
+    if request.method == 'POST':
+        contact.delete()
+        return redirect('contact_list')
+    return render(request, 'contact_confirm_delete.html', {'contact': contact})
